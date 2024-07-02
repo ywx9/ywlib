ac14d5f5832a1b145003449ba3502fb85677afe0
ywx9 <yw.ninefold@gmail.com>
2024-07-02 18:03:37 +0900

    stringable

--- test.cpp
+++ test.cpp
@@ -5,28 +5,17 @@ using namespace yw;
 
 int main() {
   try {
-    auto a1 = now();
-    std::cout << a1 << std::endl;
-    auto a2 = value(now);
-    std::cout << a2 << std::endl;
-    auto a3 = str1(now);
-    std::cout << a3 << std::endl;
-    auto a4 = str2(now);
-    std::wcout << a4 << std::endl;
-    auto a5 = string<uct1>(now);
-    auto b5 = to_wcs(a5);
-    std::wcout << b5 << std::endl;
-    // static_assert(decltype(now)::j<str1> == 2);
+    std::cout << str1(now) << std::endl;
+    std::wcout << str2(now) << std::endl;
+    std::cout << to_mbs(string_view<uct1>(now)) << std::endl;
+    std::wcout << to_wcs(string_view<uct2>(now)) << std::endl;
+    auto s = to_wcs(string_view<uct4>(now));
+    std::wcout << s << std::endl;
+    std::wcout << s.size() << std::endl;
 
 
-    for (auto a = 0x81; a < 0xa0; ++a) {
-      for (auto b = 0x40; b < 0xfd; ++b) {
-        char c[] = {a, b, 0};
-        auto d = to_wcs(c);
-      }
-    }
-
-
+    yw::log.debug(string_view<uct1>(u8"debug test"));
+    std::cout << yw::log;
   } catch (const std::exception& E) {
     std::cout << E.what();
     return -1;
