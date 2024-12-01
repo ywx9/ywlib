e8691e1efb89b33b4b7f108dce1616c24442a2b5
ywx9 <yw.ninefold@gmail.com>
2024-12-02 08:05:36 +0900

    update

--- ywlib.hpp
+++ ywlib.hpp
@@ -1,4 +1,1 @@
 #pragma once
-#include "ywstd/ywstd.hpp"
-
-namespace yw {}
--- ywstd/ywstd.hpp
+++ ywstd/ywstd.hpp
@@ -4,5 +4,11 @@ import ywstd;
 #else // for coding or compiling module
 #define _BUILD_STD_MODULE
 #include "intrin.hpp"
+#include "sjis.hpp"
 #include "std.hpp"
+#include "win.hpp"
+#endif
+
+#ifndef nat
+#define nat size_t
 #endif
